a064d77abf29c65523a99110e6efb1c0e0d878f7
ibraztect2 <ibraztech2@gmail.com>
2024-03-07 15:25:35 +0100

    initial commit

--- numpy_lesson.py
+++ numpy_lesson.py
@@ -7,6 +7,13 @@ from pandas import *
 np.random.seed(101)
 
 df = DataFrame(randn(5, 4),["A","B","C","D","E"],["W","X","Y","Z"] )
-print(df)
-pick =  df["A"]["B"]
-print(pick)
+# pick = df[["W","X"]]
+df["new"] = df["W"] + df["X"]
+# pick= df.loc(1)
+# print(pick)
+# print(df.drop("E",axis=0))
+# print(df.drop("new",axis=1,inplace=True))
+# print(df.loc["A"])
+# print(df.iloc[1])
+# print(df.loc["A","W"])
+# print(df.loc[["A","B"],["W","Y"]])
