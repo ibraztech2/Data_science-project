69591c68a1b60e8ef0929126d63d538224a43429
ibraztech2 <156426270+ibraztech2@users.noreply.github.com>
2024-04-16 14:58:49 +0100

    additional feature
    added power used in either millli watt or watt depending on its value

--- dataframe=_test.py
+++ dataframe=_test.py
@@ -1,11 +1,12 @@
 from pandas import *
+from os import *
 from numpy import *
 
 
-resistance_1 = [i for i in range(10,10000,46)]
+resistance_1 = [i for i in range(1,100,3)]
 resistance = []
 resistance_mapping = []
-resistance_2 = 500
+resistance_2 = 20
 voltage = 5
 for i in resistance_1:
     effect_r = (i * resistance_2) /(resistance_2 + i)
@@ -15,7 +16,21 @@ data_frame_index = [i for i in range(1,len(resistance) +1)]
 df= DataFrame(resistance_mapping,data_frame_index,columns=["resistance","Effective_resistance"])
 df["current"] = voltage/ df["resistance"]
 df_current  = df["current"]
+df_current_analysed = []
+for i in df_current:
+    if (i - 1) < 0:
+        i = i * 1000
+        i = f'{i:.2f} mw'
+    else:
+        i = f'{i:.3f} w'
+    df_current_analysed.append(i)
+
+
 df["power"] = (df_current **2) * voltage
 print(f" voltage {voltage}, resistance {resistance_2}")
+# print(df)
+df["df_current_analysed"] = df_current_analysed
 print(df)
-print(df.shape)
+bool_df = df["current"]
+
+# print(bool_df)guijkm,i6i8u7
